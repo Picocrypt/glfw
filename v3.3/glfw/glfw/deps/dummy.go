6310eaa9c3bc2ab1b4f5775f0f3412349c9d028f
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2024-08-30 20:30:11 -0400

    Update dummy.go

--- v3.3/glfw/glfw/deps/dummy.go
+++ v3.3/glfw/glfw/deps/dummy.go
@@ -6,7 +6,7 @@ package dummy
 
 import (
 	// Prevent go tooling from stripping out the c source files.
-	_ "github.com/HACKERALERT/glfw/v3.3/glfw/glfw/deps/glad"
-	_ "github.com/HACKERALERT/glfw/v3.3/glfw/glfw/deps/mingw"
-	_ "github.com/HACKERALERT/glfw/v3.3/glfw/glfw/deps/vs2008"
+	_ "github.com/Picocrypt/glfw/v3.3/glfw/glfw/deps/glad"
+	_ "github.com/Picocrypt/glfw/v3.3/glfw/glfw/deps/mingw"
+	_ "github.com/Picocrypt/glfw/v3.3/glfw/glfw/deps/vs2008"
 )
